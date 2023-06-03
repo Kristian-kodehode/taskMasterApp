acf89d988e665dd39eee4ea59d290d367c0a4b33
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-06-03 16:00:35 +0200

    The date and time issue to work properly

--- backup.js
+++ backup.js
@@ -1,3 +1,5 @@
+//!!!!BACKUP!!!!//
+
 "use strict";
 
 /*
@@ -29,15 +31,25 @@ const deleteButton = document.querySelector("#deleteButton");
 
 ////////////////////////
 //DECLARE
+
+const enter = 13;
+const backspace = 8;
 let myTasks = [];
 
 ////////////////////////
 //FUNCTION : UPDATE LOCALSTORAGE FOR EACH TASK ADDED
+//FEIL?
 const updateLocalStorage = () => {
-  localStorage.setItem("todo", JSON.stringify(myTasks));
+  localStorage.setItem(
+    "todo",
+    JSON.stringify(myTasks.map((task) => task.text))
+  );
 };
 
-const addTask = (text, timeStamp) => {
+////////////////////////
+//FUNCTION : ADD TASK
+
+const addTask = (text) => {
   const taskItem = document.createElement("li");
   //Takes line breaks created with "enter" into account.
   taskItem.style.whiteSpace = "pre-line";
@@ -46,6 +58,7 @@ const addTask = (text, timeStamp) => {
 
   ////////////////////////////////////
   //CREATE CHECKBOX
+
   const checkBox = document.createElement("input");
   checkBox.type = "checkbox";
   checkBox.name = "taskCheckBox";
@@ -54,26 +67,37 @@ const addTask = (text, timeStamp) => {
 
   ////////////////////////////////////
   //SPAN FOR EACH TASK
+
   const taskText = document.createElement("span");
   taskText.textContent = text;
 
   ////////////////////////////////////
   //DATE AND TIME
+  /*
+  let currentDate = new Date();
+  const timeStamp = currentDate.toLocaleString([], {
+    dateStyle: `medium`,
+    timeStyle: `medium`,
+  });
+  //Sets origin date for item
   const taskDate = document.createElement("div");
   taskDate.style.fontSize = "12px";
   taskDate.style.marginTop = "0.5em";
   taskDate.style.fontWeight = "400";
   taskDate.textContent = timeStamp;
-
+*/
   ////////////////////////////////////
   //DIRECTLY EDIT CREATED TASKTEXT
+
   taskText.addEventListener("click", () => {
     taskText.contentEditable = true;
     taskText.focus();
     updateLocalStorage();
   });
+
   ////////////////////////////////////
-  //UPDATES TASK WITH LATEST TEXT EDIT
+  //UPDATES WITH LATEST TEXT EDIT
+
   taskText.addEventListener("input", () => {
     myTasks.forEach((task) => {
       if (task.taskItem === taskItem) {
@@ -85,13 +109,15 @@ const addTask = (text, timeStamp) => {
 
   ////////////////////////////////////
   //APPENDS
+
   taskItem.appendChild(checkBox);
   taskItem.appendChild(taskText);
-  taskItem.appendChild(taskDate);
+  // taskItem.appendChild(taskDate);
   taskList.appendChild(taskItem);
 
   ////////////////////////////////////
   //CHECKBOX EVENTHANDLING w/STYLING
+
   checkBox.addEventListener("change", () => {
     if (checkBox.checked) {
       taskItem.style.backgroundColor = "#f4828c";
@@ -106,28 +132,30 @@ const addTask = (text, timeStamp) => {
 
   ////////////////////////////////////
   //CLEARS INPUT FIELD
+
   taskInput.value = "";
 
   ////////////////////////////////////
   //PUSH ITEMS TO myTask
+
   myTasks.push({
     taskItem,
     checkBox,
     text,
-    timeStamp,
+    // taskDate,
   });
 };
-
 ////////////////////////
 //FUNCTION : LOAD LOCALSTORAGE FOR EACH ITEM CREATED
 //Located here because addTask = arrow function.
+
 const loadFromLocalStorage = () => {
   const todos = JSON.parse(localStorage.getItem("todo")) || [];
 
   for (const todo of todos) {
-    addTask(todo.text, todo.timeStamp);
+    addTask(todo);
 
-    const { checkBox, taskItem } = myTasks[myTasks.length - 1];
+    const { checkBox } = myTasks[myTasks.length - 1];
 
     checkBox.addEventListener("change", () => {
       if (checkBox.checked) {
@@ -142,43 +170,65 @@ const loadFromLocalStorage = () => {
     });
   }
 };
-
 loadFromLocalStorage();
 
 ///////////////////////
 //FUNCTION : DELETE TASK
 
 const deleteTask = () => {
-  for (const { checkBox, taskItem } of myTasks) {
+  for (const { checkBox, taskItem } of myTasks)
     if (checkBox.checked) {
       taskItem.remove();
     }
-  }
   myTasks = myTasks.filter((task) => !task.checkBox.checked);
 };
 
 ///////////////////////
 //EVENT HANDLERS
 
+//
 taskInput.addEventListener("input", () => {
   addTaskBtn.disabled = taskInput.value === "";
 });
 
+//
 addTaskBtn.addEventListener("click", (event) => {
   if (taskInput.value === "") {
     event.preventDefault();
   } else {
-    const timeStamp = new Date().toLocaleString([], {
-      dateStyle: "medium",
-      timeStyle: "medium",
-    });
-    addTask(taskInput.value, timeStamp);
+    addTask(taskInput.value);
     updateLocalStorage();
     taskInput.value = "";
   }
 });
 
+//
 deleteButton.addEventListener("click", () => {
   deleteTask();
   updateLocalStorage();
 });
+
+////////////////////////
+//KEYDOWNS NOT USED BECAUSE IT DIDNT REALLY MAKE THAT MUCH SENSE.
+
+////////////////////////////////////
+//Enter keydown to add item
+
+// taskInput.addEventListener("keydown", (event) => {
+//   if (event.keyCode === enter) {
+//     event.preventDefault();
+//     addTask(taskInput.value);
+//     updateLocalStorage();
+//   }
+// });
+
+////////////////////////////////////
+//Backspace keydown to delete item
+
+// taskList.addEventListener("keydown", (event) => {
+//   if (event.keyCode === backspace) {
+//     event.preventDefault();
+//     deleteTask();
+//     updateLocalStorage();
+//   }
+// });
